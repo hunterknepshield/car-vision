f3cca683b3e1417d85362d80df936ec751c8b760
Hunter Knepshield <theragingfatcat@gmail.com>
2018-03-29 23:07:32 -0400

    Add simple trapezoid selection.

--- lane_detection.py
+++ lane_detection.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 import sys
 
 def show(name, image):
@@ -11,11 +12,37 @@ def show(name, image):
 	cv2.destroyAllWindows()
 	cv2.waitKey(1)
 
+def show_with_axes(name, image):
+	print(name)
+	plt.imshow(image)
+	plt.show()
+
+def select_trapezoid(image):
+	'''
+	Based on the image dimensions, we select from the bottom corners to
+	slightly above the center of the image.
+	'''
+	(rows, cols) = (image.shape[0], image.shape[1])
+	bl = (int(cols*.1), int(rows))
+	br = (int(cols*.9), int(rows))
+	tl = (int(cols*.45), int(rows*.5))
+	tr = (int(cols*.55), int(rows*.5))
+
+	# For debugging purposes...
+	#'''	
+	cv2.line(image, bl, br, (0, 0, 255), 10) # Red
+	cv2.line(image, br, tr, (0, 255, 0), 10) # Green
+	cv2.line(image, tr, tl, (255, 0, 0), 10) # Blue
+	cv2.line(image, tl, bl, (255, 255, 255), 10) # White
+	show_with_axes('Lines', image)
+	#'''
+
 def detect_lines(image):
 	'''
 	Works with static images. Ideally we can scale to videos easily.
 	'''
-	show('Image', image)
+	#show('Image', image)
+	select_trapezoid(image)
 
 if __name__ == '__main__':
 	for file in sys.argv[1:]:
