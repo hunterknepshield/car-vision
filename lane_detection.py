96044c0ab262b41c310bb95941efe3fd070b9338
Hunter Knepshield <theragingfatcat@gmail.com>
2018-04-23 04:25:29 -0400

    Basic line detection implemented using thresholding + histogram + interpolation.

--- lane_detection.py
+++ lane_detection.py
@@ -35,12 +35,9 @@ def get_birds_eye_view(image):
 	bird's eye view, as well as the points and the transform matrix calculated
 	to achieve it.
 
-	To undo the transformation, use
-	cv2.warpPerspective(warped, warp_matrix, \
-					    (original.shape[1], original.shape[0]), \
-						flags=cv2.WARP_INVERSE_MAP)
-	or call
-	get_original_view(warped, warp_matrix, original.shape)
+	To undo the transformation, use either
+	get_original_view(warped, warp_matrix, original.shape), or
+	cv2.warpPerspective(warped, warp_matrix, (original.shape[1], original.shape[0]), flags=cv2.WARP_INVERSE_MAP).
 	'''
 	# Generate the original anchor points.
 	(rows, cols) = (image.shape[0], image.shape[1])
@@ -48,7 +45,6 @@ def get_birds_eye_view(image):
 	bottom_right = (int(cols*.9), int(rows))
 	top_left = (int(cols*.45), int(rows*.6))
 	top_right = (int(cols*.55), int(rows*.6))
-	show_with_axes('Original', image)
 
 	# For debugging purposes...
 	'''
@@ -69,7 +65,6 @@ def get_birds_eye_view(image):
 	# Warp to bird's eye view
 	perspective_matrix = cv2.getPerspectiveTransform(original_points, warped_points)
 	warped = cv2.warpPerspective(image, perspective_matrix, (width, height))
-	show_with_axes('Warped', warped)
 	return (warped, original_points, perspective_matrix)
 
 
@@ -107,26 +102,100 @@ def superimpose(original, modified, anchors):
 	return superimposed
 
 
+def binary_histogram(binary_image, mask=None):
+	'''
+	Scans horizontally across a binary image and returns a histogram of pixel
+	count against the column.
+	'''
+	# TODO(hknepshield) fairly slow - speed up with some fancy np calls?
+	histogram = np.zeros(binary_image.shape[1], dtype=np.uint32)
+	for row in range(binary_image.shape[0]):
+		if mask is not None and np.all(mask[row, :] == 0):
+			continue
+		for col in range(binary_image.shape[1]):
+			if mask is not None and mask[row, col] == 0:
+				continue
+			if binary_image[row, col] != 0:
+				histogram[col] += 1
+	return histogram
+
+
+def find_histogram_maxima(histogram):
+	'''
+	Returns a pair (of which one or both may be None) of column numbers that
+	have local maxima. Exactly divides the histogram in half and searches on
+	either side of the dividing line. This fits the assumption that lanes will
+	never cross over to the other side.
+	'''
+	half = int(histogram.shape[0]/2)
+	left = np.argmax(histogram[:half])
+	if histogram[left] == 0:
+		left = None
+	right = np.argmax(histogram[half:]) + half
+	if histogram[right] == 0:
+		right = None
+	return (left, right)
+
+
 def detect_lines(image):
 	'''
 	Works with static images. Ideally we can scale to videos easily.
 	'''
-	#show('Image', image)
+	show_with_axes('Original', image)
 	(warped, trapezoid_points, warp_matrix) = get_birds_eye_view(image)
 	show_with_axes('Warped', warped)
 
+	'''
 	# Change white to green
 	mask = np.logical_and(warped[:,:,0] >= 200, warped[:,:,1] >= 200, warped[:,:,2] >= 200)
 	warped[mask] = (0, 255, 0)
+	'''
+
+	gray = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+	# TODO(hknepshield) blur first?
+	(thresh_num, thresholded) = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	show_with_axes('Thresholded', thresholded)
+	# Look at 50 pixel tall strips of the image and find the two lines from the
+	# peaks in the strips' histograms.
+	left_points = []
+	right_points = []
+	strip_size = 50
+	for strip_end in range(thresholded.shape[0], 0, -strip_size):
+		strip_begin = max(strip_end - strip_size, 0)
+		mask = np.zeros(thresholded.shape[:2], dtype=np.uint8)
+		mask[strip_begin:strip_end, :] = 255
+		hist = binary_histogram(thresholded, mask)
+		(lmax, rmax) = find_histogram_maxima(hist)
+		#'''
+		plt.plot(hist)
+		plt.title('Histogram for strip ({}, {})'.format(strip_begin, strip_end))
+		plt.show()
+		#'''
+		if lmax is not None:
+			point = (lmax, int((strip_begin + strip_end)/2))
+			cv2.circle(warped, point, 1, (255, 0, 0), thickness=8)
+			left_points.append(point)
+		if rmax is not None:
+			point = (rmax, int((strip_begin + strip_end)/2))
+			cv2.circle(warped, point, 1, (0, 0, 255), thickness=8)
+			right_points.append(point)
+	# Interpolate to the bottom of the image, since un-warping as-is causes a
+	# pretty large gap from the bottom to the first point.
+	bottom_row = thresholded.shape[0] - 1
+	left_points = np.array(left_points, dtype=np.int32)
+	left_line = np.polyfit(left_points[:, 1], left_points[:, 0], 2) # Quadratic
+	left_interp = np.polyval(left_line, bottom_row)
+	left_points = np.insert(left_points, 0, np.array([left_interp, bottom_row], dtype=left_points.dtype), axis=0)
+	right_points = np.array(right_points, dtype=np.int32)
+	right_line = np.polyfit(right_points[:, 1], right_points[:, 0], 2)
+	right_interp = np.polyval(right_line, bottom_row)
+	right_points = np.insert(right_points, 0, np.array([right_interp, bottom_row], dtype=right_points.dtype), axis=0)
+	cv2.polylines(warped, [left_points], False, (0, 255, 0), thickness=5)
+	cv2.polylines(warped, [right_points], False, (0, 255, 0), thickness=5)
+	show_with_axes('Lines', warped)
 
 	undone = get_original_view(warped, warp_matrix, image.shape)
 	show_with_axes('Undone', undone)
-
-	# Generally, looks like any thresholds higher than these do it as well.
-	#show_with_axes('Edges', cv2.Canny(undone, 100, 150))
-	#	for upper in range(0, 250, 25):
-	#		for lower in range(0, min(110, upper), 10):
-	#			show_with_axes('Edges, lower = {}, upper = {}'.format(lower, upper), cv2.Canny(undone, lower, upper))
 
 	superimposed = superimpose(image, undone, trapezoid_points)
 	show_with_axes('Superimposed', superimposed)
