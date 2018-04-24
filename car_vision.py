76550ead0ddd742e903691be6350f95750315786
Hunter Knepshield <theragingfatcat@gmail.com>
2018-04-23 21:00:35 -0400

    Spaces and tabs are annoying and Python makes them more annoying.

--- car_vision.py
+++ car_vision.py
@@ -11,23 +11,23 @@ from lane_detection import detect_lines
 
 #Set of supported image formats
 EXTENSIONS = set(['jpg','jpeg','jif','jfif','jp2','j2k','j2c','fpx','tif', \
-		  'tiff','pcd','png','ppm','webp','bmp','bpg','dib','wav', \
-		  'cgm','svg'])
+				  'tiff','pcd','png','ppm','webp','bmp','bpg','dib','wav', \
+				  'cgm','svg'])
 
 # Set of supported video formats
 VIDEO_EXTENSIONS = set(['mp4'])
 
 
 def perceive_road(file):
-    '''
-    Handles lane and vehicle detection then determines course of action
-    @params:
-        file: input image of road
-    @returns:
-        TBD
-    '''
-    detect_lines(cv2.imread(file), True)
-    #detect_vehicles()
+	'''
+	Handles lane and vehicle detection then determines course of action
+	@params:
+		file: input image of road
+	@returns:
+		TBD
+	'''
+	detect_lines(cv2.imread(file), True)
+	#detect_vehicles()
 
 
 def perceive_road_video(file):
@@ -48,20 +48,20 @@ def perceive_road_video(file):
 
 
 if __name__ == '__main__':
-    '''
-    Manage input, output, and program's operational flow
-    '''
-    if (len(sys.argv) < 2):
-        print('Error: No target provided')
-        sys.exit()
+	'''
+	Manage input, output, and program's operational flow
+	'''
+	if (len(sys.argv) < 2):
+		print('Error: No target provided')
+		sys.exit()
 
-    for file in sys.argv[1:]:
+	for file in sys.argv[1:]:
 		ext = file.split('.')[1].lower()
 		if os.path.isdir(file):
 			print('Error: cannot supply directory - ' + str(file))
 		elif ext in EXTENSIONS:
-            perceive_road(file)
+			perceive_road(file)
 		elif ext in VIDEO_EXTENSIONS:
 			perceive_road_video(file)
-        else:
-            print('Error: unsupported input - ' + str(file))
+		else:
+			print('Error: unsupported input - ' + str(file))
